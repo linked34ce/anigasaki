e32f5a75479a2c317cbeb8eef07219ce8591ef52
hosoyan <9797ouki@gmail.com>
2024-01-20 18:37:18 +0900

    refactor js files

--- src/episode.js
+++ src/episode.js
@@ -7,63 +7,7 @@ window.addEventListener("DOMContentLoaded", () => {
         queue = setTimeout(adjustEpisodePage, 200);
     });
 
-    function adjustEpisodePage() {
-        let title = document.getElementById("title");
-        let buttons = document.getElementById("buttons");
-        let episodes = document.getElementById("episodes");
-        let episodeTable = document.getElementById("episode-table");
-        let episodeLinks = episodes.getElementsByTagName("a");
-        let mainContent = document.getElementById("main-contents");
-
-        if (window.innerWidth < window.innerHeight * 0.74) {
-            document.getElementById("thumbnail").style.width = "80%";
-            if (title.className === "card-title text-white text-nowrap") {
-                title.className = "card-title text-white text-nowrap fs-6";
-            } else if (
-                title.className === "card-title text-black text-nowrap"
-            ) {
-                title.className = "card-title text-black text-nowrap fs-6";
-            }
-            buttons.className = "row row-cols-auto g-0";
-            mainContent.className = "mx-3 my-5 p-1 text-left";
-        } else {
-            document.getElementById("thumbnail").style.width = "26rem";
-            if (title.className === "card-title text-white text-nowrap fs-6") {
-                title.className = "card-title text-white text-nowrap";
-            } else if (
-                title.className === "card-title text-black text-nowrap fs-6"
-            ) {
-                title.className = "card-title text-black text-nowrap";
-            }
-            buttons.className = "row row-cols-auto d-block g-0";
-            mainContent.className = "m-5 p-1 text-left";
-        }
-
-        if (window.innerWidth < 370) {
-            episodes.innerHTML =
-                '<tr><td class="nav-link-hover"><a href="01.html" class="nav-link fs-6">第1話</a></td><td class="nav-link-hover"><a href="02.html" class="nav-link fs-6">第2話</a></td></tr><tr><td class="nav-link-hover"><a href="03.html" class="nav-link fs-6">第3話</a></td><td class="nav-link-hover"><a href="04.html" class="nav-link fs-6">第4話</a></td></tr><tr><td class="nav-link-hover"><a href="05.html" class="nav-link fs-6">第5話</a></td><td class="nav-link-hover"><a href="06.html" class="nav-link fs-6">第6話</a></td></tr><tr><td class="nav-link-hover"><a href="07.html" class="nav-link fs-6">第7話</a></td><td class="nav-link-hover"><a href="08.html" class="nav-link fs-6">第8話</a></td></tr><tr><td class="nav-link-hover"><a href="09.html" class="nav-link fs-6">第9話</a></td><td class="nav-link-hover"><a href="10.html" class="nav-link fs-6">第10話</a></td></tr><tr><td class="nav-link-hover"><a href="11.html" class="nav-link fs-6">第11話</a></td><td class="nav-link-hover"><a href="12.html" class="nav-link fs-6">第12話</a></td></tr><tr><td class="nav-link-hover"><a href="13.html" class="nav-link fs-6">第13話</a></td></tr>';
-            if (window.innerHeight < 605) {
-                episodeTable.className =
-                    "table table-sm table-dark border-white";
-            }
-        } else if (window.innerWidth < 570) {
-            episodes.innerHTML =
-                '<tr><td class="nav-link-hover"><a href="01.html" class="nav-link fs-5">第1話</a></td><td class="nav-link-hover"><a href="02.html" class="nav-link fs-5">第2話</a></td><td class="nav-link-hover"><a href="03.html" class="nav-link fs-5">第3話</a></td></tr><tr><td class="nav-link-hover"><a href="04.html" class="nav-link fs-5">第4話</a></td><td class="nav-link-hover"><a href="05.html" class="nav-link fs-5">第5話</a></td><td class="nav-link-hover"><a href="06.html" class="nav-link fs-5">第6話</a></td></tr><tr><td class="nav-link-hover"><a href="07.html" class="nav-link fs-5">第7話</a></td><td class="nav-link-hover"><a href="08.html" class="nav-link fs-5">第8話</a></td><td class="nav-link-hover"><a href="09.html" class="nav-link fs-5">第9話</a></td></tr><tr><td class="nav-link-hover"><a href="10.html" class="nav-link fs-5">第10話</a></td><td class="nav-link-hover"><a href="11.html" class="nav-link fs-5">第11話</a></td><td class="nav-link-hover"><a href="12.html" class="nav-link fs-5">第12話</a></td></tr><tr><td class="nav-link-hover"><a href="13.html" class="nav-link fs-5">第13話</a></td></tr>';
-        } else {
-            episodes.innerHTML =
-                '<tr><td class="nav-link-hover"><a href="01.html" class="nav-link fs-5">第1話</a></td><td class="nav-link-hover"><a href="02.html" class="nav-link fs-5">第2話</a></td><td class="nav-link-hover"><a href="03.html" class="nav-link fs-5">第3話</a></td><td class="nav-link-hover"><a href="04.html" class="nav-link fs-5">第4話</a></td><td class="nav-link-hover"><a href="05.html" class="nav-link fs-5">第5話</a></tr><tr></td><td class="nav-link-hover"><a href="06.html" class="nav-link fs-5">第6話</a></td><td class="nav-link-hover"><a href="07.html" class="nav-link fs-5">第7話</a></td><td class="nav-link-hover"><a href="08.html" class="nav-link fs-5">第8話</a></td><td class="nav-link-hover"><a href="09.html" class="nav-link fs-5">第9話</a></td><td class="nav-link-hover"><a href="10.html" class="nav-link fs-5">第10話</a></td></tr><tr><td class="nav-link-hover"><a href="11.html" class="nav-link fs-5">第11話</a></td><td class="nav-link-hover"><a href="12.html" class="nav-link fs-5">第12話</a></td><td class="nav-link-hover"><a href="13.html" class="nav-link fs-5">第13話</a></td></tr>';
-        }
-
-        for (i = 0; i < episodeLinks.length; i++) {
-            if (episodeLinks.item(i).href === location.href) {
-                episodeLinks.item(i).parentElement.id = "in-this-page";
-                episodeLinks.item(i).parentElement.innerHTML =
-                    '<a class="nav-link fs-5">第' + (i + 1) + "話</a>";
-            }
-        }
-    }
-
-    let characters = [
+    const characters = [
         "yu",
         "ayumu",
         "kasumi",
@@ -112,8 +56,8 @@ window.addEventListener("DOMContentLoaded", () => {
     }
 
     document.getElementById("songs").addEventListener("click", () => {
-        let elements = document.getElementById("main-contents").children;
-        let lyrics = document.getElementsByClassName("lyric");
+        const elements = document.getElementById("main-contents").children;
+        const lyrics = document.getElementsByClassName("lyric");
 
         for (let element of elements) {
             if (
@@ -131,8 +75,8 @@ window.addEventListener("DOMContentLoaded", () => {
     });
 
     document.getElementById("all").addEventListener("click", () => {
-        let elements = document.getElementById("main-contents").children;
-        let lyrics = document.getElementsByClassName("lyric");
+        const elements = document.getElementById("main-contents").children;
+        const lyrics = document.getElementsByClassName("lyric");
 
         for (let element of elements) {
             element.style.display = "block";
@@ -142,42 +86,98 @@ window.addEventListener("DOMContentLoaded", () => {
         }
     });
 
-    let accordionStates = [false, false];
+    const accordionStates = [false, false];
 
     if (document.getElementById("opening")) {
         document.getElementById("opening").addEventListener("click", () => {
-            openAccordion("op-expansion", 0);
+            openAccordion(accordionStates, true);
         });
     }
 
     if (document.getElementById("ending")) {
         document.getElementById("ending").addEventListener("click", () => {
-            openAccordion("ed-expansion", 1);
+            openAccordion(accordionStates, false);
         });
     }
-
-    function openAccordion(id, index) {
-        if (!accordionStates[index]) {
-            document.getElementById(id).style.animation =
-                "350ms ease-in 0s 1 normal none running expand";
-            setTimeout(() => {
-                document.getElementById(id).innerHTML =
-                    "<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' fill='currentColor' class='bi bi-dash' viewBox='0 0 12 15'><path d='M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z'/></svg>";
-                document.getElementById(id).className = "minus";
-            }, 175);
-            accordionStates[index] = true;
-        } else {
-            document.getElementById(id).style.animation =
-                "350ms ease-out 0s 1 normal none running collapse";
-            setTimeout(() => {
-                document.getElementById(id).innerHTML =
-                    "<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' fill='currentColor' class='bi bi-plus' viewBox='0 0 12 15'><path d='M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z'/></svg>";
-                document.getElementById(id).className = "plus";
-            }, 175);
-            accordionStates[index] = false;
-        }
-        setTimeout(() => {
-            document.getElementById(id).style.animation = "";
-        }, 300);
-    }
 });
+
+const adjustEpisodePage = () => {
+    const title = document.getElementById("title");
+    const buttons = document.getElementById("buttons");
+    const episodes = document.getElementById("episodes");
+    const episodeTable = document.getElementById("episode-table");
+    const episodeLinks = episodes.getElementsByTagName("a");
+    const mainContent = document.getElementById("main-contents");
+
+    if (window.innerWidth < window.innerHeight * 0.74) {
+        document.getElementById("thumbnail").style.width = "80%";
+        if (title.className === "card-title text-white text-nowrap") {
+            title.className = "card-title text-white text-nowrap fs-6";
+        } else if (title.className === "card-title text-black text-nowrap") {
+            title.className = "card-title text-black text-nowrap fs-6";
+        }
+        buttons.className = "row row-cols-auto g-0";
+        mainContent.className = "mx-3 my-5 p-1 text-left";
+    } else {
+        document.getElementById("thumbnail").style.width = "26rem";
+        if (title.className === "card-title text-white text-nowrap fs-6") {
+            title.className = "card-title text-white text-nowrap";
+        } else if (
+            title.className === "card-title text-black text-nowrap fs-6"
+        ) {
+            title.className = "card-title text-black text-nowrap";
+        }
+        buttons.className = "row row-cols-auto d-block g-0";
+        mainContent.className = "m-5 p-1 text-left";
+    }
+
+    if (window.innerWidth < 370) {
+        episodes.innerHTML =
+            '<tr><td class="nav-link-hover"><a href="01.html" class="nav-link fs-6">第1話</a></td><td class="nav-link-hover"><a href="02.html" class="nav-link fs-6">第2話</a></td></tr><tr><td class="nav-link-hover"><a href="03.html" class="nav-link fs-6">第3話</a></td><td class="nav-link-hover"><a href="04.html" class="nav-link fs-6">第4話</a></td></tr><tr><td class="nav-link-hover"><a href="05.html" class="nav-link fs-6">第5話</a></td><td class="nav-link-hover"><a href="06.html" class="nav-link fs-6">第6話</a></td></tr><tr><td class="nav-link-hover"><a href="07.html" class="nav-link fs-6">第7話</a></td><td class="nav-link-hover"><a href="08.html" class="nav-link fs-6">第8話</a></td></tr><tr><td class="nav-link-hover"><a href="09.html" class="nav-link fs-6">第9話</a></td><td class="nav-link-hover"><a href="10.html" class="nav-link fs-6">第10話</a></td></tr><tr><td class="nav-link-hover"><a href="11.html" class="nav-link fs-6">第11話</a></td><td class="nav-link-hover"><a href="12.html" class="nav-link fs-6">第12話</a></td></tr><tr><td class="nav-link-hover"><a href="13.html" class="nav-link fs-6">第13話</a></td></tr>';
+        if (window.innerHeight < 605) {
+            episodeTable.className = "table table-sm table-dark border-white";
+        }
+    } else if (window.innerWidth < 570) {
+        episodes.innerHTML =
+            '<tr><td class="nav-link-hover"><a href="01.html" class="nav-link fs-5">第1話</a></td><td class="nav-link-hover"><a href="02.html" class="nav-link fs-5">第2話</a></td><td class="nav-link-hover"><a href="03.html" class="nav-link fs-5">第3話</a></td></tr><tr><td class="nav-link-hover"><a href="04.html" class="nav-link fs-5">第4話</a></td><td class="nav-link-hover"><a href="05.html" class="nav-link fs-5">第5話</a></td><td class="nav-link-hover"><a href="06.html" class="nav-link fs-5">第6話</a></td></tr><tr><td class="nav-link-hover"><a href="07.html" class="nav-link fs-5">第7話</a></td><td class="nav-link-hover"><a href="08.html" class="nav-link fs-5">第8話</a></td><td class="nav-link-hover"><a href="09.html" class="nav-link fs-5">第9話</a></td></tr><tr><td class="nav-link-hover"><a href="10.html" class="nav-link fs-5">第10話</a></td><td class="nav-link-hover"><a href="11.html" class="nav-link fs-5">第11話</a></td><td class="nav-link-hover"><a href="12.html" class="nav-link fs-5">第12話</a></td></tr><tr><td class="nav-link-hover"><a href="13.html" class="nav-link fs-5">第13話</a></td></tr>';
+    } else {
+        episodes.innerHTML =
+            '<tr><td class="nav-link-hover"><a href="01.html" class="nav-link fs-5">第1話</a></td><td class="nav-link-hover"><a href="02.html" class="nav-link fs-5">第2話</a></td><td class="nav-link-hover"><a href="03.html" class="nav-link fs-5">第3話</a></td><td class="nav-link-hover"><a href="04.html" class="nav-link fs-5">第4話</a></td><td class="nav-link-hover"><a href="05.html" class="nav-link fs-5">第5話</a></tr><tr></td><td class="nav-link-hover"><a href="06.html" class="nav-link fs-5">第6話</a></td><td class="nav-link-hover"><a href="07.html" class="nav-link fs-5">第7話</a></td><td class="nav-link-hover"><a href="08.html" class="nav-link fs-5">第8話</a></td><td class="nav-link-hover"><a href="09.html" class="nav-link fs-5">第9話</a></td><td class="nav-link-hover"><a href="10.html" class="nav-link fs-5">第10話</a></td></tr><tr><td class="nav-link-hover"><a href="11.html" class="nav-link fs-5">第11話</a></td><td class="nav-link-hover"><a href="12.html" class="nav-link fs-5">第12話</a></td><td class="nav-link-hover"><a href="13.html" class="nav-link fs-5">第13話</a></td></tr>';
+    }
+
+    for (let i = 0; i < episodeLinks.length; i++) {
+        if (episodeLinks.item(i).href === location.href) {
+            episodeLinks.item(i).parentElement.id = "in-this-page";
+            episodeLinks.item(i).parentElement.innerHTML =
+                '<a class="nav-link fs-5">第' + (i + 1) + "話</a>";
+        }
+    }
+};
+
+const openAccordion = (accordionStates, isOp) => {
+    const index = isOp ? 0 : 1;
+    const id = (isOp ? "op" : "ed") + "-expansion";
+
+    if (!accordionStates[index]) {
+        document.getElementById(id).style.animation =
+            "350ms ease-in 0s 1 normal none running expand";
+        setTimeout(() => {
+            document.getElementById(id).innerHTML =
+                "<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' fill='currentColor' class='bi bi-dash' viewBox='0 0 12 15'><path d='M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z'/></svg>";
+            document.getElementById(id).className = "minus";
+        }, 175);
+        accordionStates[index] = true;
+    } else {
+        document.getElementById(id).style.animation =
+            "350ms ease-out 0s 1 normal none running collapse";
+        setTimeout(() => {
+            document.getElementById(id).innerHTML =
+                "<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' fill='currentColor' class='bi bi-plus' viewBox='0 0 12 15'><path d='M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z'/></svg>";
+            document.getElementById(id).className = "plus";
+        }, 175);
+        accordionStates[index] = false;
+    }
+    setTimeout(() => {
+        document.getElementById(id).style.animation = "";
+    }, 300);
+};
