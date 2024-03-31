687e4c3dcceaf7b2522e4c712ee01951af737e73
hosoyan <9797ouki@gmail.com>
2024-04-01 00:38:03 +0900

    create #01 file and fixed links

--- src/js/functions/adjustment/const/episode.js
+++ src/js/functions/adjustment/const/episode.js
@@ -1,3 +1,3 @@
-export const LATEST_EPISODE = 13;
+export const LATEST_EPISODE = 14;
 export const NUM_OF_EPISODES = 26;
 export const NUM_OF_EPISODES_PER_SEASON = 13;
--- src/ts/functions/adjustment/const/episode.ts
+++ src/ts/functions/adjustment/const/episode.ts
@@ -1,3 +1,3 @@
-export const LATEST_EPISODE: number = 13;
+export const LATEST_EPISODE: number = 14;
 export const NUM_OF_EPISODES: number = 26;
 export const NUM_OF_EPISODES_PER_SEASON: number = 13;
