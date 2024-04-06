64fdabab319f4bb8cde8c022f73a3e1b01259242
hosoyan <9797ouki@gmail.com>
2024-04-06 23:31:01 +0900

    control scroll for smart devices

--- src/js/functions/adjustment/controlScroll.js
+++ src/js/functions/adjustment/controlScroll.js
@@ -1,8 +1,14 @@
 export const controlScroll = () => {
     let navOpened = false;
+    let clientY;
     const navContent = document.getElementById("nav-content");
-    const handleScroll = (e) => {
+    const handleScrollPage = (e) => {
         e.preventDefault();
+    };
+    const handleTouchMovePage = (e) => {
+        if (e.cancelable) {
+            e.preventDefault();
+        }
     };
     const handleScrollNavigation = (e) => {
         const isScrolledToTop = navContent?.scrollTop < 1;
@@ -17,25 +23,44 @@ export const controlScroll = () => {
             e.stopPropagation();
         }
     };
+    const handleTouchMoveNavigation = (e) => {
+        const isScrolledToTop = navContent?.scrollTop < 1;
+        const isScrolledToBottom = Math.abs(navContent?.scrollHeight -
+            navContent?.clientHeight -
+            navContent?.scrollTop) < 1;
+        const deltaY = clientY - e.touches[0].clientY;
+        if ((isScrolledToTop && deltaY < 0) ||
+            (isScrolledToBottom && deltaY > 0)) {
+            if (e.cancelable) {
+                e.preventDefault();
+            }
+        }
+        else {
+            e.stopPropagation();
+        }
+    };
     document.getElementById("nav-button")?.addEventListener("click", () => {
         navOpened = !navOpened;
+        navContent?.addEventListener("touchstart", (e) => {
+            clientY = e.touches[0].clientY;
+        }, false);
         navContent?.addEventListener("wheel", handleScrollNavigation, {
             passive: false,
         });
-        navContent?.addEventListener("touchmove", handleScrollNavigation, {
+        navContent?.addEventListener("touchmove", handleTouchMoveNavigation, {
             passive: false,
         });
         if (navOpened) {
-            document.addEventListener("wheel", handleScroll, {
+            document.addEventListener("wheel", handleScrollPage, {
                 passive: false,
             });
-            document.addEventListener("touchmove", handleScroll, {
+            document.addEventListener("touchmove", handleTouchMovePage, {
                 passive: false,
             });
         }
         else {
-            document.removeEventListener("wheel", handleScroll);
-            document.removeEventListener("touchmove", handleScroll);
+            document.removeEventListener("wheel", handleScrollPage);
+            document.removeEventListener("touchmove", handleTouchMovePage);
         }
     });
 };
--- src/ts/functions/adjustment/controlScroll.ts
+++ src/ts/functions/adjustment/controlScroll.ts
@@ -1,12 +1,19 @@
 export const controlScroll = () => {
     let navOpened = false;
+    let clientY: number;
     const navContent = document.getElementById("nav-content");
 
-    const handleScroll = (e: Event) => {
+    const handleScrollPage = (e: WheelEvent) => {
         e.preventDefault();
     };
 
-    const handleScrollNavigation = (e: WheelEvent | TouchEvent) => {
+    const handleTouchMovePage = (e: TouchEvent) => {
+        if (e.cancelable) {
+            e.preventDefault();
+        }
+    };
+
+    const handleScrollNavigation = (e: WheelEvent) => {
         const isScrolledToTop = (navContent?.scrollTop as number) < 1;
         const isScrolledToBottom =
             Math.abs(
@@ -25,26 +32,57 @@ export const controlScroll = () => {
         }
     };
 
+    const handleTouchMoveNavigation = (e: TouchEvent) => {
+        const isScrolledToTop = (navContent?.scrollTop as number) < 1;
+        const isScrolledToBottom =
+            Math.abs(
+                (navContent?.scrollHeight as number) -
+                    (navContent?.clientHeight as number) -
+                    (navContent?.scrollTop as number),
+            ) < 1;
+
+        const deltaY = clientY - e.touches[0].clientY;
+
+        if (
+            (isScrolledToTop && deltaY < 0) ||
+            (isScrolledToBottom && deltaY > 0)
+        ) {
+            if (e.cancelable) {
+                e.preventDefault();
+            }
+        } else {
+            e.stopPropagation();
+        }
+    };
+
     document.getElementById("nav-button")?.addEventListener("click", () => {
         navOpened = !navOpened;
+
+        navContent?.addEventListener(
+            "touchstart",
+            (e) => {
+                clientY = e.touches[0].clientY;
+            },
+            false,
+        );
 
         navContent?.addEventListener("wheel", handleScrollNavigation, {
             passive: false,
         });
-        navContent?.addEventListener("touchmove", handleScrollNavigation, {
+        navContent?.addEventListener("touchmove", handleTouchMoveNavigation, {
             passive: false,
         });
 
         if (navOpened) {
-            document.addEventListener("wheel", handleScroll, {
+            document.addEventListener("wheel", handleScrollPage, {
                 passive: false,
             });
-            document.addEventListener("touchmove", handleScroll, {
+            document.addEventListener("touchmove", handleTouchMovePage, {
                 passive: false,
             });
         } else {
-            document.removeEventListener("wheel", handleScroll);
-            document.removeEventListener("touchmove", handleScroll);
+            document.removeEventListener("wheel", handleScrollPage);
+            document.removeEventListener("touchmove", handleTouchMovePage);
         }
     });
 };
