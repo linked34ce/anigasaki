1014a7d2d4b5e6b7b1fe2d9f7f8c94778596a152
hosoyan <9797ouki@gmail.com>
2024-09-08 20:59:07 +0900

    fix internal links

--- src/js/functions/adjustment/const/episode.js
+++ src/js/functions/adjustment/const/episode.js
@@ -1,4 +1,4 @@
-export const LATEST_EPISODE = 14;
+export const LATEST_EPISODE = 15;
 export const NUM_OF_EPISODES = 26;
 export const NUM_OF_EPISODES_PER_SEASON = 13;
 export const SEASONS = ['s1', 's2'];
